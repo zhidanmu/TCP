cbd89b4481df88d00ea7ade94a87e0808a750af6
hocyi <hocyisemail@qq.com>
2023-12-19 16:11:24 +0800

    rdt2.2

---
+++ checkRecvData.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+for /f "delims= skip=1" %%i in ('certUtil -hashfile recvData.txt MD5') do (
+	set recv=%%i
+	goto next
+)
+:next
+for /f "delims= skip=1" %%i in ('certUtil -hashfile recvData_standard.txt MD5') do (
+	set standard=%%i
+	goto compare
+)
+:compare
+if %recv%==%standard% (echo same) else (echo error)
+
+pause 
